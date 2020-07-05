3c49458539962477890c2e6cd7e94e16f25c6719
Eliel Maciel <elielmsilvatec@gmail.com>
2020-07-05 12:25:54 -0400

    arquivo pdf

--- rec.php
+++ rec.php
@@ -1,28 +1,48 @@
-<?php 
-ob_start();
 
-$nome = filter_input(INPUT_POST, 'nome');
-$cpf = filter_input(INPUT_POST, 'cpf');
-?>
 
-<h1> <?php echo $nome ?> </h1>
-<h4><?php echo $cpf; ?> </h4>
 
-<?php 
-$html = ob_get_contents();
-ob_end_clean();
 
-require 'vendor/autoload.php';
 
-$mpdf = new Mpdf\Mpdf();
-$mpdf->WriteHTML($html);
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	
+	<title>Curruclum ideal</title>
+</head>
+<body>
+	<?php 
+	ob_start();
+	// AQUI ESTÁ RECEBENDO OS DADOS DO FORMULÁRIO
+	$nome = filter_input(INPUT_POST, 'nome');
+	$cpf = filter_input(INPUT_POST, 'cpf');
+	?>
+	<!-- CÓDIGO DO CORPO DO HTML -->
+	<h1 style="color: red; text-align: center;"> <?php echo $nome ?> </h1>
+	<h4><?php echo $cpf; ?> </h4>
 
-// I - abra no browser (padrão)
-// D - Faz o download
-// F - Salva no servidor
 
-$mpdf->output();
 
-$nome_arquivo = substr(strrchr($link, '/'),1);
-?>
- 
+	<!-- CONVERTENDO O HTML EM PDF -->
+	<?php 
+	$html = ob_get_contents();
+	ob_end_clean();
+	require 'vendor/autoload.php';
+	$mpdf = new Mpdf\Mpdf();
+	$mpdf->WriteHTML($html);
+	$mpdf->output();
+	$nome_arquivo = substr(strrchr($link, '/'),1);
+	?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
